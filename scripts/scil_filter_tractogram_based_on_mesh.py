902e2ce579034ffa5dfebde10bde121345aa7f60
grahamlittlephd <graham.little.phd@gmail.com>
2022-09-13 16:16:30 -0400

    intersect mesh script

--- scripts/scil_filter_tractogram_based_on_mesh.py
+++ scripts/scil_filter_tractogram_based_on_mesh.py
@@ -43,6 +43,7 @@ from dipy.io.stateful_tractogram import StatefulTractogram
 
 import open3d as o3d
 
+
 def _build_arg_parser():
     p = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
                                 description=__doc__)
@@ -61,21 +62,23 @@ def _build_arg_parser():
                    '(i.e. mesh_roi left_striatum.ply both_ends include).')
 
     p.add_argument('--dist_thr', type=float, default=0.1,
-                    help='distance in mm, if mesh_roi used points within this threshold are considered in the mesh. [%(default)s]')
-    
+                   help='distance in mm, if mesh_roi used points within this threshold are considered in the mesh. [%(default)s]')
+
     p.add_argument('--intersect_min_angle', type=float, default=0.0,
-                    help='minimum angle in radians, if intersect_roi used remove streamlines that\n'
-                    'intersect the mesh at an anlge smaller than this value. [%(default)s]')
+                   help='minimum angle in radians, if intersect_roi used remove streamlines that\n'
+                   'intersect the mesh at an anlge smaller than this value. [%(default)s]')
     p.add_argument('--intersect_max_angle', type=float, default=0.5,
-                    help='maximum angle in radians, if intersect_roi used remove streamlines that\n'
-                    'intersect the mesh at an angle larger than this value. [%(default)s]')
+                   help='maximum angle in radians, if intersect_roi used remove streamlines that\n'
+                   'intersect the mesh at an angle larger than this value. [%(default)s]')
+    p.add_argument('--intersect_dist', type=float, default=3.0,
+                   help='distance in mm, only look this far from ends of streamlines for mesh intersections. [%(default)s]')
 
     p.add_argument('--no_empty', action='store_true',
-                    help='Do not write file if there is no streamline.')
-    
+                   help='Do not write file if there is no streamline.')
+
     p.add_argument('--display_counts', action='store_true',
-                    help='Print streamline count before and after filtering')
-    
+                   help='Print streamline count before and after filtering')
+
     p.add_argument('--save_rejected', metavar='FILENAME',
                    help='Save rejected streamlines to output tractogram.')
 
@@ -83,8 +86,9 @@ def _build_arg_parser():
     add_overwrite_arg(p)
     add_verbose_arg(p)
     add_json_args(p)
-    
+
     return p
+
 
 def prepare_filtering_list(parser, args):
     roi_opt_list = []
@@ -104,7 +108,7 @@ def prepare_filtering_list(parser, args):
     for roi_opt in roi_opt_list:
 
         _, _, filter_mode, filter_criteria = roi_opt
-        
+
         if filter_mode not in ['any', 'all', 'either_end', 'both_ends']:
             parser.error('{} is not a valid option for filter_mode'.format(
                 filter_mode))
@@ -114,6 +118,7 @@ def prepare_filtering_list(parser, args):
                 filter_criteria))
 
     return roi_opt_list, only_filtering_list
+
 
 def streamline_endpoints_in_mesh(sft, target_mesh, both_ends=False, dist_thr=0.1):
     """
@@ -143,22 +148,23 @@ def streamline_endpoints_in_mesh(sft, target_mesh, both_ends=False, dist_thr=0.1
 
     # TODO definitely a faster way to do this, the casting is fast but the for loop sucks
     # probably do a reshape get all signed distances and then reshape back
-    
+
     # use ray casting to find distance from each point in tractogram to mesh
     filter_list = []
-    for i in range(0,len(sft.streamlines)):
+    for i in range(0, len(sft.streamlines)):
         # Get first and last endpoint of each streamline
         query_points = [sft.streamlines[i][0], sft.streamlines[i][-1]]
 
         # Cast rays and get distance (negative values if inside)
         signed_distance = np.array(scene.compute_signed_distance(query_points))
-        
-        # Check if both endpoints are within mesh
+
         if both_ends:
+            # Check if both endpoints are within mesh
             if signed_distance[0] < dist_thr and signed_distance[1] < dist_thr:
                 filter_list.append(i)
-        # Check if either endpoint is within mesh
+        
         else:
+            # Check if either endpoint is within mesh
             if signed_distance[0] < dist_thr or signed_distance[1] < dist_thr:
                 filter_list.append(i)
 
@@ -193,15 +199,15 @@ def streamlines_in_mesh(sft, target_mesh, all_in=False, dist_thr=0.1):
 
     # TODO definitely a faster way to do this, the casting is fast but the for loop sucks
     # probably do a reshape get all signed distances and then reshape back
-    
+
     # use ray casting to find distance from each point in tractogram to mesh
     filter_list = []
-    for i in range(0,len(sft.streamlines)):
+    for i in range(0, len(sft.streamlines)):
         query_points = sft.streamlines[i]
 
-        # Cast rays and get distance (negative values if inside) 
+        # Cast rays and get distance (negative values if inside)
         signed_distance = np.array(scene.compute_signed_distance(query_points))
-        
+
         # decide if this streamline should be filtered
         if all_in:
             if np.all(signed_distance < dist_thr):
@@ -209,6 +215,59 @@ def streamlines_in_mesh(sft, target_mesh, all_in=False, dist_thr=0.1):
                 print(signed_distance)
         else:
             if np.any(signed_distance < dist_thr):
+                filter_list.append(i)
+
+    return filter_list
+
+def streamlines_intersect_with_mesh(sft, target_mesh, both_ends=False, intersect_min_angle=0.0, intersect_max_angle=0.5, intersect_dist_thr=3.0):
+    """
+    Parameters
+    ----------
+    sft : StatefulTractogram
+        StatefulTractogram containing the streamlines to segment.
+    target_mesh : o3d.geometry.TriangleMesh
+        Mesh oriented RAS mm in which.
+    both_ends : bool
+        If True, both end points must be within mesh.
+    intersect_min_angle : float
+        Minimum angle between streamline and mesh surface in radians.
+    intersect_max_angle : float
+        Maximum angle between streamline and mesh surface in radians.
+    intersect_dist : float
+        Distance in mm from end of streamline to search for intersection.
+    Returns
+    -------
+    ids : list
+        Ids of the streamlines that pass filter (any or all).
+    """
+    # tractogram should be in RASmm same with mesh
+    sft.to_rasmm()
+    sft.to_corner()
+
+    # Legacy mesh needed for ray casting
+    legacy_mesh = o3d.t.geometry.TriangleMesh.from_legacy(target_mesh)
+    scene = o3d.t.geometry.RaycastingScene()
+    scene.add_triangles(legacy_mesh)
+
+    # TODO definitely a faster way to do this, the casting is fast but the for loop sucks
+    # probably do a reshape get all signed distances and then reshape back
+
+    # use ray casting to find distance from each point in tractogram to mesh
+    filter_list = []
+    for i in range(0, len(sft.streamlines)):
+        # Get first and last endpoint of each streamline
+        query_points = [sft.streamlines[i][0], sft.streamlines[i][-1]]
+
+        # Cast rays and get distance (negative values if inside)
+        signed_distance = np.array(scene.compute_signed_distance(query_points))
+
+        # Check if both endpoints are within mesh
+        if both_ends:
+            if signed_distance[0] < dist_thr and signed_distance[1] < dist_thr:
+                filter_list.append(i)
+        # Check if either endpoint is within mesh
+        else:
+            if signed_distance[0] < dist_thr or signed_distance[1] < dist_thr:
                 filter_list.append(i)
 
     return filter_list
@@ -225,6 +284,8 @@ def filter_mesh_roi(sft, mesh, filter_type, is_exclude, dist_thr=0.1):
         One of the 3 following choices, 'any', 'all', 'either_end', 'both_ends'.
     is_exclude: bool
         Value to indicate if the ROI is an AND (false) or a NOT (true).
+    dist_thr : float
+        Distance threshold in mm to consider a point in the mesh.
     Returns
     -------
     new_sft: StatefulTractogram
@@ -232,24 +293,24 @@ def filter_mesh_roi(sft, mesh, filter_type, is_exclude, dist_thr=0.1):
     ids: list
         Ids of the streamlines passing through the mask.
     """
-    
+
     line_based_indices = []
     if filter_type in ['any', 'all']:
         # Point based filtering
         line_based_indices = streamlines_in_mesh(sft, mesh,
-                                                all_in=filter_type == 'all', 
-                                                dist_thr=dist_thr)
+                                                 all_in=filter_type == 'all',
+                                                 dist_thr=dist_thr)
     else:
         # End point filtering
         line_based_indices = streamline_endpoints_in_mesh(sft, mesh,
-                                                both_ends = filter_type == 'both_ends', 
-                                                dist_thr=dist_thr)
+                                                          both_ends=filter_type == 'both_ends',
+                                                          dist_thr=dist_thr)
 
     # If the 'exclude' option is used, the selection is inverted
     if is_exclude:
         line_based_indices = np.setdiff1d(range(len(sft)),
                                           np.unique(line_based_indices))
-    
+
     line_based_indices = np.asarray(line_based_indices, dtype=np.int32)
 
     # From indices to sft
@@ -263,6 +324,61 @@ def filter_mesh_roi(sft, mesh, filter_type, is_exclude, dist_thr=0.1):
         data_per_point=data_per_point)
 
     return new_sft, line_based_indices
+
+
+def filter_mesh_intersect(sft, mesh, filter_type, is_exclude, intersect_min_angle=0.0, intersect_max_angle=0.5,intersect_dist=3.0):
+    """
+    Parameters
+    ----------
+    sft : StatefulTractogram
+        StatefulTractogram containing the streamlines to segment.
+    mesh : o3d.geometry.TriangleMesh
+        Mesh oriented RAS mm.
+    filter_type: str
+        One of the 2 following choices, 'either_end', 'both_ends'.
+    is_exclude: bool
+        Value to indicate if the ROI is an AND (false) or a NOT (true).
+    intersect_min_angle : float
+        Minimum angle in radians between the streamline and the mesh.
+    intersect_max_angle : float
+        Maximum angle in radians between the streamline and the mesh.
+    Returns
+    -------
+    new_sft: StatefulTractogram
+        Filtered sft.
+    ids: list
+        Ids of the streamlines passing through the mask.
+    """
+
+    line_based_indices = []
+    if filter_type in ['either_end', 'both_ends']:
+        line_based_indices = streamlines_intersect_with_mesh(sft, mesh,
+                                                             both_ends=filter_type == 'both_ends',
+                                                             intersect_min_angle=intersect_min_angle,
+                                                             intersect_max_angle=intersect_max_angle,
+                                                             intersect_dist=intersect_dist)
+    else:
+        raise ValueError("Filter type not recognized for mesh intersection")
+
+    # If the 'exclude' option is used, the selection is inverted
+    if is_exclude:
+        line_based_indices = np.setdiff1d(range(len(sft)),
+                                          np.unique(line_based_indices))
+
+    line_based_indices = np.asarray(line_based_indices, dtype=np.int32)
+
+    # From indices to sft
+    streamlines = sft.streamlines[line_based_indices]
+    data_per_streamline = sft.data_per_streamline[line_based_indices]
+    data_per_point = sft.data_per_point[line_based_indices]
+
+    new_sft = StatefulTractogram.from_sft(
+        streamlines, sft,
+        data_per_streamline=data_per_streamline,
+        data_per_point=data_per_point)
+
+    return new_sft, line_based_indices
+
 
 def main():
     parser = _build_arg_parser()
@@ -289,7 +405,7 @@ def main():
     for i, roi_opt in enumerate(roi_opt_list):
         logging.debug("Preparing filtering from option: {}".format(roi_opt))
         curr_dict = {}
-        
+
         filter_type, filter_arg, filter_mode, filter_criteria = roi_opt
 
         curr_dict['filename'] = os.path.abspath(filter_arg)
@@ -306,11 +422,18 @@ def main():
             filtered_sft, kept_ids = filter_mesh_roi(sft, mesh,
                                                      filter_mode, is_exclude, args.dist_thr)
 
-        if filter_type == 'mesh_intersection':
+        if filter_type == 'mesh_intersect':
+            if filter_mode == 'any' or filter_mode == 'all':
+                raise ValueError(
+                    "Only 'either_end' or 'both_ends' are valid for mesh_intersect")
+
             mesh = o3d.io.read_triangle_mesh(filter_arg)
 
-            #filtered_sft, kept_ids = filter_mesh_intersection(sft, mesh,
-            #filter_mode, is_exclude, args.intersect_min_angle, args.intersect_max_angle)
+            filtered_sft, kept_ids = filter_mesh_intersect(sft, mesh,
+                                                           filter_mode, is_exclude, 
+                                                           args.intersect_min_angle, 
+                                                           args.intersect_max_angle,
+                                                           args.intersect_dist)
 
         logging.debug('The filtering options {0} resulted in '
                       '{1} streamlines'.format(roi_opt, len(filtered_sft)))
